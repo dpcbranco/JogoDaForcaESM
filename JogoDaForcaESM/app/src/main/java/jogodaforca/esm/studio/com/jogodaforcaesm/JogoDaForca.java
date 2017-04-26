991a2404a3dcbba340a4a46900e4cc19b0e17a7d
Shikado <dani.hyuuga@hotmail.com>
2017-04-26 18:35:53 -0300

    Android Project added

---
+++ JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/JogoDaForca.java
@@ -0,0 +1,13 @@
+package jogodaforca.esm.studio.com.jogodaforcaesm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoDaForca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_da_forca);
+    }
+}
