6a039d569bf749a326503b368a631d9448cba9ba
Shikado <dani.hyuuga@hotmail.com>
2017-04-26 20:41:00 -0300

    Jogo da Forca updated. New functions added

--- JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/JogoDaForca.java
+++ JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/JogoDaForca.java
@@ -1,13 +1,100 @@
 package jogodaforca.esm.studio.com.jogodaforcaesm;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import jogodaforca.esm.studio.com.jogodaforcaesm.jogador.Jogador;
+
 public class JogoDaForca extends AppCompatActivity {
+
+    private int n_jogadores;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private List<Jogador> jogadores = new ArrayList<Jogador>();
+    String palavra_secreta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_da_forca);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void setN_jogadores(int n) {
+        if (n > 0 && n <= 8) {
+            this.n_jogadores = n;
+        } else {
+            throw new RuntimeException("Nº de jogadores inválido");
+        }
+    }
+
+    private void criarJogadores(){
+        for (int i = 0; i < n_jogadores; i++) {
+            jogadores.add(0, new Jogador());
+        }
+    }
+
+    //recebe as palavras dos jogadores que não estão jogando e sorteia uma delas
+    private String setPalavraSecreta (String Palavra_J1, String Palavra_J2){
+        Random sorteio = new Random(1);
+        if (sorteio.nextInt() == 0){
+            return Palavra_J1;
+        }
+        else{
+            return Palavra_J2;
+        }
+    }
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("JogoDaForca Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
+
