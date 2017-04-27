365bab4fa2f6a4e63890357002bc2253c4d87002
Shikado <dani.hyuuga@hotmail.com>
2017-04-26 21:03:50 -0300

    Build update

--- JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/Jogador/Jogador.java
+++ JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/Jogador/Jogador.java
@@ -1,9 +1,10 @@
-package jogodaforca.esm.studio.com.jogodaforcaesm.Jogador;
+package jogodaforca.esm.studio.com.jogodaforcaesm.jogador;
 
 /**
  * Created by Shikado on 26/04/2017.
  */
 
 public class Jogador {
-    
+    boolean morto = false;
+
 }
