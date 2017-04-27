21c5107186de4fb92a73ecf3bee98d93e7fec6b5
Shikado <dani.hyuuga@hotmail.com>
2017-04-26 22:01:45 -0300

    Tentativa de implementação do boneco da forca

--- JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/jogador/Forca.java
+++ JogoDaForcaESM/app/src/main/java/jogodaforca/esm/studio/com/jogodaforcaesm/jogador/Forca.java
@@ -1,9 +1,26 @@
 package jogodaforca.esm.studio.com.jogodaforcaesm.jogador;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Shikado on 26/04/2017.
  */
 
 class Forca {
+    List<Shape> boneco = new ArrayList<Shape>();
+
+    public Forca() {
+        boneco.add(0, new Shape() {
+            @Override
+            public void draw(Canvas canvas, Paint paint) {
+                canvas.drawOval(50,50,50,50,paint);
+            }
+        });
+    }
 
 }
