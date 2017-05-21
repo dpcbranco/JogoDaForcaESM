94f7859149206dde878d0db67dd4a3a01ec35b17
Daniel <dani.hyuuga@hotmail.com>
2017-05-20 22:34:35 -0300

    Thread para conexão ao servidor (não pode ocorrer na Thread Principal)

---
+++ JogoDaForcaESM/app/src/main/java/com/pucsp/esm/jogodaforcaesm/jogador/Conexao.java
@@ -0,0 +1,29 @@
+package com.pucsp.esm.jogodaforcaesm.jogador;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by Nerds on 20/05/2017.
+ */
+
+public class Conexao extends AsyncTask <String, Void, Socket> {
+
+    Socket client = null;
+
+    @Override
+    protected Socket doInBackground(String... params) {
+
+        try {
+            client = new Socket(params[0], 12345);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return client;
+    }
+
+}
+
