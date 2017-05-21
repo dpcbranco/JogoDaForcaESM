57273ed207f65e9385aa67530a26b1f0d7096dfb
Daniel <dani.hyuuga@hotmail.com>
2017-05-20 22:17:23 -0300

    Recepção de letras enviadas pelo celular

--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -3,28 +3,43 @@ package servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.DataInputStream;
+import java.io.EOFException;
 
 public class Servidor {
 	
 	ServerSocket servidor;
+	ArrayList <Socket> jogadores = new ArrayList<>();
+	ArrayList <DataInputStream> entrada = new ArrayList<>(); 
 	
 	public Servidor () throws IOException {
+		
 		servidor = new ServerSocket(12345);
 		System.out.println("Porta 12345 aberta!");
+		Socket novo_jogador;
+		DataInputStream nova_entrada;
 
-		Socket cliente = servidor.accept();
+		novo_jogador = servidor.accept();	
+		nova_entrada = new DataInputStream (novo_jogador.getInputStream());
+				
 		System.out.println("Nova conexão com o cliente " +   
-				cliente.getInetAddress().getHostAddress()
+				novo_jogador.getInetAddress().getHostAddress()
 				);
-
-		Scanner s = new Scanner(cliente.getInputStream());
-		while (s.hasNextLine()) {
-			System.out.println(s.nextLine());
+		
+		//jogadores.add(novo_jogador);
+		//entrada.add(nova_entrada);
+		
+		try{	
+			while (novo_jogador.isConnected()){
+				System.out.println(nova_entrada.readUTF());
+			}
+		}catch (EOFException e){
+			System.out.println("Encerrando server");
+			novo_jogador.close();
+			servidor.close();
 		}
-
-		s.close();
-		servidor.close();
-		cliente.close();
+		
+		//jogadores.get(0).close();
 	}
 }
