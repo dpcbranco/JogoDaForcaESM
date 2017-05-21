94fb5166e15c1b99aba043189840b12dc4930c6e
Daniel <dani.hyuuga@hotmail.com>
2017-05-20 22:27:12 -0300

    Sem alteração

--- JogoDaForcaESM/app/src/main/java/com/pucsp/esm/jogodaforcaesm/activities/TelaPrincipal.java
+++ JogoDaForcaESM/app/src/main/java/com/pucsp/esm/jogodaforcaesm/activities/TelaPrincipal.java
@@ -1,8 +1,8 @@
 package com.pucsp.esm.jogodaforcaesm.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +11,7 @@ import com.pucsp.esm.jogodaforcaesm.R;
 
 import java.net.Socket;
 
-public class TelaPrincipal extends Activity {
+public class TelaPrincipal extends AppCompatActivity {
 
     private Socket comunicacao;
 
@@ -26,7 +26,10 @@ public class TelaPrincipal extends Activity {
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String endereco_str = campo_sala.getText().toString();
+                String nickname = campo_nickname.getText().toString();
                 startActivity(new Intent(TelaPrincipal.this, JogoDaForcaESM.class));
+
             }
         });
 
